c16ed6fa5761f1c2409b1e6c867c13cee5164dd4
mariusz_m <azamennace@gmail.com>
2016-11-26 02:17:13 +0100

    Add project files.

---
+++ NetStatus/NetStatus/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Connectivity;
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+            if (CrossConnectivity.Current.IsConnected)
+                MainPage = new NetStatus.MainPage();
+            else
+                MainPage = new NetStatus.Page1();
+		}
+
+        
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+		}
+
+        private void Current_ConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+            if (e.IsConnected)
+                MainPage = new NetStatus.MainPage();
+            else
+                MainPage = new NetStatus.Page1();
+        }
+
+        protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ NetStatus/NetStatus/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+	public partial class Page1 : ContentPage
+	{
+        Label noNetworkLabel;
+
+		public Page1 ()
+		{
+            noNetworkLabel = new Label();
+            noNetworkLabel.Text = "There is no network!";
+            noNetworkLabel.HorizontalOptions = LayoutOptions.CenterAndExpand;
+            noNetworkLabel.TextColor = Color.FromRgb(0x40, 0x40, 0x40);
+            Content = noNetworkLabel;
+			InitializeComponent ();
+            this.BackgroundColor = Color.FromRgb(0xF0, 0xF0,0xF0);
+		}
+	}
+}
